d5cb9943edc7a05afcb3e9a84d94de6d5af53f66
mohan <mohanr3054@gmail.com>
2022-10-11 21:03:05 +0530

    app change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from './Sidebar';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../src/sb-admin-2.css';
+import '../src/fontawesome-free/css/all.min.css';
+import Topbar from './Topbar';
+import User from './User';
+import Dashboard from './Dashboard';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+   <div id="wrapper">
+      <Sidebar></Sidebar>
+      <div id="content-wrapper" class="d-flex flex-column">
+        <div id="content"><Topbar></Topbar>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard/>}></Route>
+          <Route path="/Users" element={<User/>}></Route>
+        </Routes>
+       
+        </div>
+      </div>
+
     </div>
+  </BrowserRouter>
+    
   );
 }
 
---
+++ src/User.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function User() {
+  return (
+    <div>
+      <img src='https://media.istockphoto.com/photos/taj-mahal-mausoleum-in-agra-picture-id1146517111?k=20&m=1146517111&s=612x612&w=0&h=vHWfu6TE0R5rG6DJkV42Jxr49aEsLN0ML-ihvtim8kk='/>
+    </div>
+  )
+}
+
+export default User
